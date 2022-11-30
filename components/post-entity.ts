ef9b49a3f3ceab1a88adc25e21c17de7612d6284
Michael Puckett <michaelcpuckett@gmail.com>
2022-11-29 21:11:07 -0500

    remove like count didnt work

--- components/post-entity.ts
+++ components/post-entity.ts
@@ -16,15 +16,6 @@ export class PostEntity extends LitElement {
   @property({ type: String, reflect: true, attribute: 'entity-id' })
   private entityId = '';
 
-  @property({ type: Number })
-  private likesCount: number = -1;
-
-  @property({ type: Number })
-  private sharesCount: number = -1;
-
-  @property({ type: Number })
-  private repliesCount: number = -1;
-
   @property({ type: Object })
   private entity: AP.Entity;
 
@@ -37,36 +28,6 @@ export class PostEntity extends LitElement {
     .then(res => res.json())
     .then(entity => {
       this.entity = entity;
-
-      fetch(`/proxy?resource=${this.entity.likes}`, {
-        headers: {
-          'Accept': 'application/activity+json'
-        }
-      })
-      .then(res => res.json())
-      .then(likes => {
-        this.likesCount = likes.totalItems;
-      });
-      
-      fetch(`/proxy?resource=${this.entity.shares}`, {
-        headers: {
-          'Accept': 'application/activity+json'
-        }
-      })
-      .then(res => res.json())
-      .then(shares => {
-        this.sharesCount = shares.totalItems;
-      });
-      
-      fetch(`/proxy?resource=${this.entity.replies}`, {
-        headers: {
-          'Accept': 'application/activity+json'
-        }
-      })
-      .then(res => res.json())
-      .then(replies => {
-        this.repliesCount = replies.totalItems;
-      });
     })
     .catch(() => {
       this.entity = 'Not found.';
@@ -103,21 +64,6 @@ export class PostEntity extends LitElement {
         ${attachmentHtml}
         ${unsafeHTML(this.entity.content)}
       </figure>
-      <p>
-        <a href=${this.entity.likes}>
-        ${this.likesCount > -1 ? this.likesCount : '... Loading'} Likes
-        </a>
-      </p>
-      <p>
-        <a href=${this.entity.shares}>
-        ${this.sharesCount > -1 ? this.sharesCount : '... Loading'} Shares
-        </a>
-      </p>
-      <p>
-        <a href=${this.entity.shares}>
-          ${this.repliesCount > -1 ? this.repliesCount : '... Loading'} Replies
-        </a>
-      </p>
     `;
   }
 }
