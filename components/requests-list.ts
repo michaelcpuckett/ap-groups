9ff2c8a7eb1bc8e65d13fc578e5836ebb1483d02
Michael Puckett <michael@puckett.contact>
2022-12-03 15:03:28 -0500

    fix request list

--- components/requests-list.ts
+++ components/requests-list.ts
@@ -12,7 +12,7 @@ export class RequestsList extends LitElement {
     }
   `];
 
-  @property({type: Array})
+  @property({type: Object})
   private requestIds?: string[];
 
   @property({type: String, attribute: 'primary-action'})
