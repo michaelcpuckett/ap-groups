2542b10552c3bb3d7a735803d7e2fa74dd7db415
Michael Puckett <michael@puckett.contact>
2022-12-05 10:40:45 -0500

    fixup

--- components/home-page.ts
+++ components/home-page.ts
@@ -325,14 +325,18 @@ export class HomePage extends LitElement {
               <div class="regions">
                 ${this.shared.map(({ id, context, object }) => html`
                   <div class="region">
+                    ${context !== object ? html`
+                      <p>
+                        <post-entity
+                          entity-id=${context}>
+                        </post-entity>
+                      </p>
+                    ` : nothing}
                     <figure>
                       <post-entity
                         entity-id=${object}>
                       </post-entity>
                     </figure>
-                    <p>
-                      <post-entity entity-id=${context}></post-entity>
-                    </p>
                     <button
                       @click=${() => this.undoAnnounce(id)}
                       class="button"
