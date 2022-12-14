637faa25e0445485199789f0631a260bd5fe6ad3
Michael Puckett <michael@puckett.contact>
2022-12-14 10:24:06 -0500

    try to auth with mastodon

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ import * as nunjucks from 'nunjucks';
 import { AP } from 'activitypub-core-types';
 import * as path from 'path';
 import { AssertionError } from 'assert';
+import * as Formidable from 'formidable';
+import FormData from 'form-data';
+import * as cookie from 'cookie';
+import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } from 'simple-oauth2';
 
 const app = express.default();
 app.use(express.static(path.resolve(__dirname, '../static')));
@@ -93,6 +97,173 @@ function assertIsGroup(entity: AP.Entity): asserts entity is AP.Group {
         db: mongoDbAdapter,
       },
     });
+  
+  const OAUTH_CALLBACK_URL = 'https://chirp.social/mastodon-ouath-callback';
+
+  app.get('/mastodon-oauth', async (req, res) => {
+    try {
+      const cookies = cookie.parse(req.headers.cookie ?? '');
+      const userId = await firebaseAuthAdapter.getUserIdByToken(cookies.__session ?? '');
+    
+      if (!userId) {
+        throw new Error('Not authorized.');
+      }
+    
+      const domain = new URL(LOCAL_DOMAIN + req.url).searchParams.get('domain');
+
+      if (!domain) {
+        throw new Error('No domain.');
+      }
+
+      const callbackUrl = `${OAUTH_CALLBACK_URL}?domain=${domain}`;
+      let clientId = await mongoDbAdapter.findStringValueById('client_id', domain);
+      let clientSecret = await mongoDbAdapter.findStringValueById('client_secret', domain);
+
+      if (!clientId || !clientSecret) {
+        const body = new FormData();
+        body.append('client_name', 'chirp.social');
+        body.append('redirect_uris', callbackUrl);
+        body.append('website', 'https://chirp.social');
+    
+        mongoDbAdapter.fetch(`https://${domain}/api/v1/apps`, {
+          method: 'POST',
+          body,
+        })
+        .then((res: IncomingMessage): Promise<{
+          client_id: string;
+          client_secret: string;
+        }> => {
+          const form = Formidable.default();
+    
+          return new Promise((resolve, reject) => {
+            form.parse(res, (err, fields) => {
+              if (err) {
+                reject(err);
+                return;
+              }
+    
+              resolve(fields as {
+                client_id: string;
+                client_secret: string;
+              });
+            });
+          });
+        })
+        .then(async({
+          client_id,
+          client_secret,
+        }) => {
+          await mongoDbAdapter.saveString('client_id', domain, client_id);
+          await mongoDbAdapter.saveString('client_secret', domain, client_secret);
+
+          clientId = client_id;
+          clientSecret = client_secret;
+        });
+      }
+
+      const config = {
+        client: {
+          id: clientId,
+          secret: clientSecret,
+        },
+        auth: {
+          tokenHost: 'https://api.oauth.com'
+        }
+      };
+      
+      const client = new AuthorizationCode(config);
+
+      const authorizationUri = client.authorizeURL({
+        redirect_uri: callbackUrl,
+        scope: 'read',
+      });
+
+      res.redirect(authorizationUri);
+    } catch (error) {
+      console.log(error);
+      res.write(error.toString());
+      res.statusCode = 500;
+      res.end();
+    }
+  });
+
+  app.get('/mastodon-ouath-callback', async (req, res) => {
+    const { code, domain } = req.query;
+    const callbackUrl = `${OAUTH_CALLBACK_URL}?domain=${domain}`;
+
+    const tokenParams = {
+      code: `${code}`,
+      redirect_uri: callbackUrl,
+      scope: 'read',
+    };
+
+    let clientId = await mongoDbAdapter.findStringValueById('client_id', `${domain}`);
+    let clientSecret = await mongoDbAdapter.findStringValueById('client_secret', `${domain}`);
+
+    const config = {
+      client: {
+        id: clientId,
+        secret: clientSecret,
+      },
+      auth: {
+        tokenHost: 'https://api.oauth.com'
+      }
+    };
+    
+    const client = new AuthorizationCode(config);
+
+    try {
+      const accessToken = await client.getToken(tokenParams);
+
+      if (!accessToken.token) {
+        throw new Error('No token');
+      }
+
+      const user = await mongoDbAdapter.fetch(`https://${domain}/api/v1/accounts/verify_credentials`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${accessToken.token}`,
+        },
+      });
+
+      if (!user) {
+        throw new Error('No user found');
+      }
+
+      const accountUrl = user.url;
+
+      const cookies = cookie.parse(req.headers.cookie ?? '');
+
+      const userId = await firebaseAuthAdapter.getUserIdByToken(cookies.__session ?? '');
+    
+      if (!userId) {
+        throw new Error('Not authorized.');
+      }
+    
+      await mongoDbAdapter.db.collection('managers').replaceOne(
+        {
+          _id: userId,
+        },
+        {
+          $push: {
+            managers: {
+              $each: [accountUrl],
+            },
+          },
+        },
+        {
+          upsert: true,
+        },
+      );
+
+      res.redirect('/home');
+    } catch (error) {
+      console.log('Access Token Error', error.message);
+      res.write(error.toString());
+      res.statusCode = 500;
+      res.end();
+    }
+  });
     
   app.use(
     activityPub({
