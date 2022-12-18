875883c21dabe0ac3d9eb7904b08b88557b1ff71
Michael Puckett <michaelcpuckett@gmail.com>
2022-12-18 11:27:15 -0500

    add search

---
+++ components/search-users-form.ts
@@ -0,0 +1,136 @@
+import {LitElement, html, css, nothing} from 'lit';
+import {customElement, property, query, state} from 'lit/decorators';
+import { repeat } from 'lit/directives/repeat';
+import { AP } from 'activitypub-core-types';
+import { baseCss } from './base-css';
+
+@customElement('search-users-form')
+export class SearchUsersForm extends LitElement {
+  static styles = [baseCss, css`
+    :host {
+      display: block;
+    }
+
+    a {
+      color: var(--text-on-light-background-color);
+      display: block;
+    }
+  `];
+
+  @property({ type: String, reflect: true, attribute: 'entity-id' })
+  private entityId = '';
+ 
+  @property({ type: Array })
+  private usernames: string[] = [];
+
+  @state()
+  private results = [];
+
+  @state()
+  private hasFetchedUsernames = false;
+
+  private fetchUsernames() {
+    if (this.hasFetchedUsernames) {
+      return;
+    }
+
+    this.hasFetchedUsernames = true;
+
+    if (!this.entityId) {
+      return;
+    }
+
+    fetch(new URL(this.entityId).pathname, {
+      headers: {
+        'Accept': 'application/activity+json'
+      }
+    })
+      .then(res => res.json())
+      .then(async entity => {
+        this.usernames = (await Promise.all(entity.orderedItems
+          .map(async (activityReference: AP.ActivityReference) => {
+            const activity = await fetch(`${activityReference}`, {
+              headers: {
+                'Accept': 'application/activity+json'
+              }
+            }).then(res => res.json())
+        
+            if (activity.type !== 'Create') {
+              return null;
+            }
+
+            const createActivity = activity as AP.Create;
+            const actor = createActivity.object as AP.Actor;
+            return actor.preferredUsername;
+          })))
+          .filter((result: string|null) => typeof result == 'string');
+      })
+      .catch(() => {
+        this.usernames = [];
+      });
+  }
+
+  private handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+  }
+
+  private handleSearchInput(event: InputEvent) {
+    const target = event.currentTarget;
+
+    if (!(target instanceof HTMLInputElement)) {
+      return;
+    }
+
+    this.fetchUsernames();
+    this.results = this.getResults(target.value.toLowerCase());
+  }
+
+  private getResults(value: string) {
+    if (!value) {
+      return [];
+    }
+
+    return this.usernames.filter(username => username.toLowerCase().startsWith(value));
+  }
+
+  render() {
+    return html`
+      <form
+        @submit=${this.handleSubmit}
+        novalidate>
+        <div role="table">
+          <label role="row" class="label">
+            <span role="columnheader" class="label-text">
+              Search Groups by Username
+            </span>
+            <span role="cell">
+              <input
+                placeholder="grouphandle"
+                @input=${this.handleSearchInput}
+                type="search"
+              />
+              <div role="panel">
+                ${repeat(this.results.slice(0, 5), (username) => {
+                  return html`
+                    <a href=${`/@${username}/outbox?page=1&current`}>
+                      ${username}
+                    </a>
+                  `;
+                })}
+              </div>
+            </span>
+          </label>
+        </div>
+        <button type="submit" class="button">
+          Send Post
+        </button>
+      </form>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "search-users-form": SearchUsersForm;
+  }
+}
