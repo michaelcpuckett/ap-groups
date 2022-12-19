4447ecd45fdc5028047e176fdeca2e616912f62a
Michael Puckett <michaelcpuckett@gmail.com>
2022-12-18 19:00:34 -0500

    fix directory entry

--- components/directory-entry.ts
+++ components/directory-entry.ts
@@ -37,6 +37,39 @@ export class DirectoryEntry extends LitElement {
   @state()
   private followersCount = -1;
 
+  /**
+  * @see https://stackoverflow.com/a/67338038/938822
+  */
+  private fromNow(date: Date, nowDate = Date.now(), rft = new Intl.RelativeTimeFormat('en', { numeric: "auto" })) {
+      const SECOND = 1000;
+      const MINUTE = 60 * SECOND;
+      const HOUR = 60 * MINUTE;
+      const DAY = 24 * HOUR;
+      const WEEK = 7 * DAY;
+      const MONTH = 30 * DAY;
+      const YEAR = 365 * DAY;
+      const intervals = [
+          { ge: YEAR, divisor: YEAR, unit: 'year' },
+          { ge: MONTH, divisor: MONTH, unit: 'month' },
+          { ge: WEEK, divisor: WEEK, unit: 'week' },
+          { ge: DAY, divisor: DAY, unit: 'day' },
+          { ge: HOUR, divisor: HOUR, unit: 'hour' },
+          { ge: MINUTE, divisor: MINUTE, unit: 'minute' },
+          { ge: 30 * SECOND, divisor: SECOND, unit: 'seconds' },
+          { ge: 0, divisor: 1, text: 'just now' },
+      ];
+      const now = new Date(nowDate).getTime();
+      const diff = now - (typeof date === 'object' ? date : new Date(date)).getTime();
+      const diffAbs = Math.abs(diff);
+      for (const interval of intervals) {
+          if (diffAbs >= interval.ge) {
+              const x = Math.round(Math.abs(diff) / interval.divisor);
+              const isFuture = diff < 0;
+              return interval.unit ? rft.format(isFuture ? x : -x, interval.unit) : interval.text;
+          }
+      }
+  }
+
   override firstUpdated() {
     const url = new URL(this.entityId);
     const isLocal = url.hostname === window.location.hostname;
@@ -162,8 +195,8 @@ export class DirectoryEntry extends LitElement {
           <dt>
             Added
           </dt>
-          <dd>
-            ${this.entity.published}
+          <dd style="text-transform: capitalize">
+            ${this.fromNow(new Date(this.entity.published))}
           </dd>
           <dt>
             Posts
