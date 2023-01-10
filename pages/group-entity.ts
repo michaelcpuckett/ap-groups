550c47d8ce680d2ea7e85fb8acbc43376e30cba4
Michael Puckett <michael@puckett.contact>
2023-01-10 12:28:48 -0500

    remove search users form

--- pages/group-entity.ts
+++ pages/group-entity.ts
@@ -4,7 +4,6 @@ import '../components/actor-entity';
 import '../components/directory-entry';
 import '../components/group-entity-details';
 import '../components/pagination-nav';
-import '../components/search-users-form';
 
 import '../components/follow-activity-entity';
 import '../components/filter-follow-activities';
